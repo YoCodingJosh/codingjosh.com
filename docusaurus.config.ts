dafacefcc4ddb9d102827db4a9be2a9bdd55cadf
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2023-12-15 10:51:43 -0600

    auto detect dark/light mode 💡

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,6 +42,11 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
