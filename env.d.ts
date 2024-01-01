ccd9d34f1b33be80325fa3836fd6dbbdc39a91b8
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-01-01 11:27:33 -0600

    fix environment variables

--- env.d.ts
+++ env.d.ts
@@ -2,13 +2,15 @@ declare global {
     namespace NodeJS {
       interface ProcessEnv {
         [key: string]: string | undefined;
+        NEXT_PUBLIC_TURNSTILE_SITE_KEY: string;
+        TURNSTILE_SECRET_KEY: string;
         // The KV Namespace binding type used here comes
         // from `@cloudflare/workers-types`, in order to
         // use it like so, make sure that you have installed
         // the package as a dev dependency and you have added
         // it to your `tsconfig.json` file under
         // `compilerOptions.types`.
-        MY_KV: KVNamespace;
+        GLOBAL_CONFIG: KVNamespace;
       }
     }
   }
