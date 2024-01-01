d898d342ccb7c957d1d064cc3c1a2b26e6b686b4
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-01-01 17:45:14 -0600

    stuff

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
 
+// import Navbar from "./Navbar";
+
 export default function Nav() {
   return (
     <Navbar fluid rounded>
       <NavbarBrand as={Link} href="/">
         { /* eslint-disable-next-line @next/next/no-img-element */ }
-        <img src="/hidamari_sketch_yuno_by_graphicsmith_d4bxvho-pre-resized.png" className="mr-3 h-6 sm:h-9" alt="Cool Yunocchi" />
+        <img src="/images/hidamari_sketch_yuno_by_graphicsmith_d4bxvho-pre-resized.png" className="mr-3 h-6 sm:h-9" alt="Cool Yunocchi" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">CodingJosh</span>
       </NavbarBrand>
       <NavbarToggle />
@@ -22,5 +24,6 @@ export default function Nav() {
         <NavbarLink href="#">Contact</NavbarLink>
       </NavbarCollapse>
     </Navbar>
+    // <Navbar />
   );
 };
