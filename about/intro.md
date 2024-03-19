bc17d72e500edd79c344cb08dea101e5620e2f7b
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-03-19 14:56:14 -0500

    content update

--- about/intro.md
+++ about/intro.md
@@ -8,14 +8,16 @@ hide_table_of_contents: true
 
 Hello there! I'm Josh, a software engineer from Kansas City.
 
-I love writing code and learning new things!
+I'm passionate about writing code and constantly learning new things. Whether it's crafting user-friendly software or exploring the latest technologies, I'm always up for the challenge.
+
+In my coding journey, I strive to create solutions that people not only use but enjoy using. Collaboration is key, and I find fulfillment in empowering others to reach their full potential.
 
 ## Quick Info
 
-* Passionate about coding
-* [ISFP-T](https://www.16personalities.com/isfp-personality)
-* Loves learning new things - especially about software dev and tech
-* Quasi-college dropout
-  * have Associate (2 year), but no Bachelor's (4 year)
-* Loves anime, manga, and Japanese culture
-* Loves junk food - especially pizza and pop
+* Completed an Associate degree but opted out of pursuing a Bachelor's.
+* Passionate about coding and dedicated to crafting meaningful software experiences.
+* Proud ISFP-T ([learn more](https://www.16personalities.com/isfp-personality)), blending creativity with technical expertise.
+* Enthusiastic about anime, manga, and Japanese culture.
+* Indulges in junk food, with a soft spot for pizza and soda.
+* Finds joy in playing retro games and exploring nostalgic adventures.
+
--- src/components/WorkHistoryItem/index.tsx
+++ src/components/WorkHistoryItem/index.tsx
@@ -41,21 +41,40 @@ interface WorkHistoryItemProps {
    * The tech skills that were used in the job.
    */
   skills?: string[];
+
+  /**
+   * If the job was a contract job.
+   * 
+   * Really just displays (Contract) after the job title and contract ended after the end date.
+   */
+  wasContract?: boolean;
 }
 
 const WorkHistoryItem: React.FC<PropsWithChildren<WorkHistoryItemProps>> = (props) => {
   const [showDetails, setShowDetails] = useState(false);
 
+  const startDate = new Date(props.startDate);
+  const endDate = props.endDate ? new Date(props.endDate) : new Date();
+  const totalMonths = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
+  const years = Math.floor(totalMonths / 12);
+  const months = Math.ceil(totalMonths % 12);
+
+  let dateString = (years > 0 ? `${years} year${(years > 1 ? 's' : '')}` : '') + (years > 0 && months > 0 ? ', ' : '');
+  dateString += months > 0 ? `${months} month` + (months > 1 ? 's' : '') : '';
+
   return (
     <div className={styles.workHistoryItem}>
-      <h3 className={styles.jobTitle}>{props.title}</h3>
+      <h3 className={styles.jobTitle}>{props.title + (props.wasContract && ' (Contract)' || '')}</h3>
       {props.companyName && <p>{props.companyName}</p>}
       {props.children && <div className={styles.detailsButton} onClick={() => setShowDetails(!showDetails)}>
         <h1 className={`chevron ${showDetails ? 'down' : 'right'}`} ></h1>
       </div>}
       <p>
         <span>
-          {props.startDate} &mdash; {props.endDate || 'Present'}
+          {props.startDate} &mdash; {`${props.endDate || 'Present'} ${(props.endDate && props.wasContract) ? ' (Contract Ended) ' : ''}`} 
+        </span>
+        <span>
+          { `(${dateString})` }
         </span>
       </p>
       {props.industry && <p>Industry: {props.industry}</p>}
