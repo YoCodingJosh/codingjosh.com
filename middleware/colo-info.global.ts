0627581ee28d149380bd84e7bf59b2d2cb730e92
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-07-10 19:58:36 -0500

    Rollback middleware fix

--- middleware/colo-info.global.ts
+++ middleware/colo-info.global.ts
@@ -2,11 +2,11 @@ import { useLocationStore } from "~/stores/location";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   // call the colo-info API to get the data center the request was routed to
-  const colo = await $fetch<ColoInfoResponse>('/api/colo-info');
+  const { data: colo } = await useFetch<ColoInfoResponse>('/api/colo-info');
 
   // set the data center in the Pinia store
   const locationStore = useLocationStore();
-  locationStore.setLocation(colo);
+  locationStore.setLocation(colo.value);
 
   // continue the navigation
   return;
